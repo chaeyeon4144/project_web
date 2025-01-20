0f8eb2f5ca89d1a60d26633493d1423724ae049f
chae yeon <odora4144@gmail.com>
2025-01-20 15:52:37 +0900

    메인 중단 완성

--- js/style.js
+++ js/style.js
@@ -30,4 +30,16 @@ window.onload = function () {
       },
     },
   });
+  var swiper = new Swiper(".main_btSwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    },
+  });
 };
