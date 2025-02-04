fd4a84a5e4b3672fd8d28d20ddedbc30e09e00b8
chae yeon <odora4144@gmail.com>
2025-02-04 18:09:44 +0900

    js 모달창 진행중

--- js/style.js
+++ js/style.js
@@ -2,13 +2,13 @@ window.onload = function () {
   // itemSwiper 코드
   var swiper = new Swiper(".visual_swiper", {
     loop: true,
-   
+
     centeredSlides: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
-   
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -25,7 +25,6 @@ window.onload = function () {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-     
     },
   });
   var swiper = new Swiper(".main_btSwiper", {
@@ -38,7 +37,6 @@ window.onload = function () {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-     
     },
   });
   // 스크롤 시 메뉴바 색상 변경
@@ -51,4 +49,19 @@ window.onload = function () {
       navbar.classList.remove("scrolled");
     }
   });
+  // 햄버거 메뉴바 클릭시 메뉴바
+  let menuBtn = document.querySelector("ham_bar");
+  let menu = document.querySelector(".mobile_menu");
+  menuBtn.addEventListener("click", function () {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+    } else {
+      menu.classList.add("open");
+    }
+  });
+  // place 팝업창 닫기
+  let button = document.querySelector(".btn-close")
+  button.addEventListener("click" , function(){
+    
+  })
 };
