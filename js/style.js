59076bfe1015f797d7ca61405f16a9c9b41786d5
chae yeon <odora4144@gmail.com>
2025-02-04 17:37:20 +0900

    네이게이션 아이콘 크기 반응형 완료

--- js/style.js
+++ js/style.js
@@ -2,16 +2,13 @@ window.onload = function () {
   // itemSwiper 코드
   var swiper = new Swiper(".visual_swiper", {
     loop: true,
-    spaceBetween: 30,
+   
     centeredSlides: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+   
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -21,25 +18,27 @@ window.onload = function () {
   var swiper = new Swiper(".main_topSwiper", {
     direction: "vertical",
     loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
+     
     },
   });
   var swiper = new Swiper(".main_btSwiper", {
     direction: "vertical",
     loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
+     
     },
   });
   // 스크롤 시 메뉴바 색상 변경
